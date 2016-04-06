87e07abc68c1b302b83ef7beea5699461be9c9bf
CJ <dennischoi@Denniss-MacBook.local>
2016-04-06 17:02:21 -0400

    Refractoring to save new pictures in database

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -8,10 +8,25 @@ class PicturesController < ApplicationController
   end
 
   def new
+    @picture = Picture.new
   end
 
   def create
-    render :text => "Saving a picture. URLL #{params[:url]}, Title: #{params[:title]}, Artist: #{params[:artist]}"
+    @picture = Picture.new(picture_params)
+    if @picture.save
+      # if save was succussful if statement will go to index.html.erb
+      redirect_to pictures_url
+    else
+      # otherwise render the view associated with the action :new (i.e. new.html.erb)
+
+      render :new
+    # render :text => "Saving a picture. URLL #{params[:url]}, Title: #{params[:title]}, Artist: #{params[:artist]}"
+    # commented the previous version of create which was only rendering text provided
   end
 
+  private
+  def picture_params
+    params.require(:picture).permit(:artist, :title, :url)
+  end
+  
 end
